d040e517299fa9b3f40c239253c249268b7ec62b
Frantisek Bartos <f.bartos96@gmail.com>
2021-07-16 17:34:43 +0200

    more informative error message when the JAGS module fails to load

--- R/fit-and-marglik.R
+++ R/fit-and-marglik.R
@@ -42,6 +42,9 @@
     # assess the model fit and deal with errors
     if(inherits(fit, "error")){
 
+      if(grepl("Unknown function", fit$message))
+        stop("The RoBMA module could not be loaded. Check whether the RoBMA package was installed correctly and whether 'RoBMA::RoBMA.private$module_location' contains path to the RoBMA JAGS module.")
+
       fit            <- list()
       converged      <- FALSE
       has_posterior  <- FALSE
