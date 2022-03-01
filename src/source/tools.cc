f50d50ffe760346ba4b2cc249343e1ee57ad1401
FBartos <f.bartos96@gmail.com>
2022-03-01 10:14:43 +0100

    more memory cleaning

--- src/source/tools.cc
+++ src/source/tools.cc
@@ -105,8 +105,8 @@ double log_std_constant_onesided(double const *x, double const *const_mu, double
   // iterate over all subspaces created by the cutpoints
   for(int i = 0; i < pow(J, K); i++){
 
-	// reset the current weight
-	temp_log_weight = 0;
+  	// reset the current weight
+  	temp_log_weight = 0;
 
     // assign proper upper and lower cutopoints & weight
     for(int k = 0; k < K; k++){
@@ -132,19 +132,21 @@ double log_std_constant_onesided(double const *x, double const *const_mu, double
       }
     }
 
-	// get the current weighted probability
-	double temp_prob = cpp_mnorm_cdf(&temp_lower[0], &temp_upper[0], &temp_infin[0], &mu[0], &sigma_stdev[0], &sigma_corr[0], K);
-	// check and skip negative numbers due to numerical imprecisions for very low probabilities
-	if(temp_prob > 0){
-	  std_constant += exp(log(temp_prob) + temp_log_weight);
-	}
+  	// get the current weighted probability
+  	double temp_prob = cpp_mnorm_cdf(&temp_lower[0], &temp_upper[0], &temp_infin[0], &mu[0], &sigma_stdev[0], &sigma_corr[0], K);
+  	// check and skip negative numbers due to numerical impressions for very low probabilities
+  	if(temp_prob > 0){
+  	  std_constant += exp(log(temp_prob) + temp_log_weight);
+  	}
 
-
-	// increase index for the next itteration
+	  // increase index for the next iteration
     increase_index( &index_weights[0], K-1, J-1);
   }
 
   // clean the memory
+  delete[] sigma_stdev;
+  delete[] sigma_corr;
+  delete[] mu;
   delete[] temp_lower;
   delete[] temp_upper;
   delete[] temp_infin;
