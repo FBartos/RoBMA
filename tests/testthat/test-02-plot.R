ffeac61e5f37bba1dec2308a1a00ea63b05c7eeb
František Bartoš <f.bartos96@gmail.com>
2026-01-11 09:55:01 +0100

    update figure plots tests

--- tests/testthat/test-02-plot.R
+++ tests/testthat/test-02-plot.R
@@ -53,6 +53,7 @@ test_that("Prior and posterior distributions for model parameters", {
 
   # publication bias parameters
   for (name in names(fits)) {
+    set.seed(1)
     if (RoBMA:::.is_PET(fits[[name]])) {
       vdiffr::expect_doppelganger(paste0("base_pp_PET_", name), function() plot(fits[[name]], parameter = "PET", prior = TRUE))
       vdiffr::expect_doppelganger(paste0("ggplot_pp_PET_", name), plot(fits[[name]], parameter = "PET", prior = TRUE, plot_type = "ggplot"))
@@ -86,5 +87,60 @@ test_that("Prior and posterior distributions for model parameters", {
 
 })
 
+test_that("Customization of prior and posterior distributions for model parameters", {
 
-# test plot customization
+  ### simple parameter
+  name <- "bcg_meta-analysis"
+
+  # no prior
+  vdiffr::expect_doppelganger("baseplot_pp_no_prior", function() plot(fits[[name]], "mu", prior = FALSE))
+  vdiffr::expect_doppelganger("ggplot_pp_no_prior", plot(fits[[name]], "mu", prior = FALSE, plot_type = "ggplot"))
+
+  # change range
+  vdiffr::expect_doppelganger("baseplot_pp_range", function() plot(fits[[name]], "mu", prior = TRUE, xlim = c(-1, 1), ylim = c(0, 5)))
+  vdiffr::expect_doppelganger("ggplot_pp_range", plot(fits[[name]], "mu", prior = TRUE, plot_type = "ggplot", xlim = c(-1, 1), ylim = c(0, 5)))
+
+  # change aesthetics
+  vdiffr::expect_doppelganger("baseplot_pp_aesthetics", function() plot(fits[[name]], "mu", prior = TRUE, lwd = 3, lty = 3, col = "blue", dots_prior = list(lwd = 3, lty = 1, col = "red")))
+  vdiffr::expect_doppelganger("ggplot_pp_aesthetics", plot(fits[[name]], "mu", prior = TRUE, plot_type = "ggplot", lwd = 2, lty = 3, col = "blue", dots_prior = list(lwd = 3, lty = 1, col = "red")))
+
+
+  ### PET-PEESE
+  name <- "dat.lehmann2018-PET"
+  set.seed(1)
+
+  # no prior
+  vdiffr::expect_doppelganger("baseplot_pp_PETPEESE_no_prior", function() plot_PETPEESE(fits[[name]], prior = FALSE))
+  vdiffr::expect_doppelganger("ggplot_pp_PETPEESE_no_prior", plot_PETPEESE(fits[[name]], prior = FALSE, plot_type = "ggplot"))
+
+  # change range
+  vdiffr::expect_doppelganger("baseplot_pp_PETPEESE_range", function() plot_PETPEESE(fits[[name]], prior = TRUE, xlim = c(0, 0.5), ylim = c(-3, 3)))
+  vdiffr::expect_doppelganger("ggplot_pp_PETPEESE_range", plot_PETPEESE(fits[[name]], prior = TRUE, plot_type = "ggplot", xlim = c(0, 0.5), ylim = c(-3, 3)))
+
+  # change aesthetics
+  vdiffr::expect_doppelganger("baseplot_pp_PETPEESE_aesthetics", function() plot_PETPEESE(fits[[name]], prior = TRUE, lwd = 3, lty = 3, col = "blue", col.fill = scales::alpha("blue", 0.20),
+                                                                               dots_prior = list(lwd = 3, lty = 1, col = "red", col.fill = scales::alpha("red", 0.20))))
+  vdiffr::expect_doppelganger("ggplot_pp_PETPEESE_aesthetics", plot_PETPEESE(fits[[name]], prior = TRUE, plot_type = "ggplot", lwd = 2, lty = 3, col = "blue", col.fill = scales::alpha("blue", 0.20),
+                                                                  dots_prior = list(lwd = 3, lty = 1, col = "red", col.fill = scales::alpha("red", 0.20))))
+
+
+  ### weight function
+  name <- "dat.lehmann2018-3PSM"
+  set.seed(1)
+
+  # no prior
+  vdiffr::expect_doppelganger("baseplot_pp_weightfunction_no_prior", function() plot_weightfunction(fits[[name]], prior = FALSE))
+  vdiffr::expect_doppelganger("ggplot_pp_weightfunction_no_prior", plot_weightfunction(fits[[name]], prior = FALSE, plot_type = "ggplot"))
+
+  # change range
+  vdiffr::expect_doppelganger("baseplot_pp_weightfunction_range", function() plot_weightfunction(fits[[name]], prior = TRUE, rescale_p_values = FALSE))
+  vdiffr::expect_doppelganger("ggplot_pp_weightfunction_range", plot_weightfunction(fits[[name]], prior = TRUE, plot_type = "ggplot", rescale_p_values = FALSE))
+
+  # change aesthetics
+  vdiffr::expect_doppelganger("baseplot_pp_weightfunction_aesthetics", function() plot_weightfunction(fits[[name]], prior = TRUE, lwd = 3, lty = 3, col = "blue", col.fill = scales::alpha("blue", 0.20),
+                                                                                        dots_prior = list(lwd = 3, lty = 1, col = "red", col.fill = scales::alpha("red", 0.20))))
+  vdiffr::expect_doppelganger("ggplot_pp_weightfunction_aesthetics", plot_weightfunction(fits[[name]], prior = TRUE, plot_type = "ggplot", lwd = 2, lty = 3, col = "blue", col.fill = scales::alpha("blue", 0.20),
+                                                                           dots_prior = list(lwd = 3, lty = 1, col = "red", col.fill = scales::alpha("red", 0.20))))
+
+})
+
