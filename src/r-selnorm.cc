b2bb91a6aafb89d0f77c6da7f870bb55025e9858
František Bartoš <f.bartos96@gmail.com>
2026-08-14 11:53:32 +0200

    Refactor native likelihood sources into focused fragments

--- src/r-selnorm.cc
+++ src/r-selnorm.cc
@@ -17,7 +17,12 @@ extern "C" double Rf_dnorm4(double, double, double, int);
 
 // Private implementation fragments share one anonymous namespace and are kept in
 // this order so helper symbols stay internal to this translation unit.
+namespace {
 #include "r-selnorm-common.cc.inc"
+#include "r-selnorm-funnel-common.cc.inc"
+#include "r-selnorm-cluster.cc.inc"
+}
+
 #include "r-selnorm-loglik.cc.inc"
 #include "r-selnorm-kernel.cc.inc"
 #include "r-selnorm-funnel-zcurve.cc.inc"
